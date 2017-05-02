fccd5ea77a5c143ba929232262aef93ddbe05b59
Alexander V. Zinin <zinin@lanit.ru>
2017-05-02 15:40:29 +0300

    small bugfix

--- XadesDemo/Infrastructure/Constants.cs
+++ XadesDemo/Infrastructure/Constants.cs
@@ -2,13 +2,15 @@
 {
     internal static class Constants
     {
+        private const string envelopNamespaceCondition = "namespace-uri()='http://schemas.xmlsoap.org/soap/envelope/'";
+
         internal const string GisServicesConfigSectionName = "GisServicesConfig";
         internal const string XadesConfigSectionName = "signingConfig";
 
         internal const string SignElementName = "sign-element";
-        internal const string SoapContentXpath = "soap:Envelope/soap:Body/*";
-        internal const string SoapBodyXpath = "soap:Envelope/soap:Body";
-        internal const string SoapHeaderXpath = "soap:Envelope/soap:Header";
+        internal const string SoapContentXpath = "*[local-name()='Envelope' and " + envelopNamespaceCondition + "]/*[local-name()='Body' and " + envelopNamespaceCondition + "]/*";
+        internal const string SoapBodyXpath = "*[local-name()='Envelope' and " + envelopNamespaceCondition + "]/*[local-name()='Body' and " + envelopNamespaceCondition + "]";
+        internal const string SoapHeaderXpath = "*[local-name()='Envelope' and " + envelopNamespaceCondition + "]/*[local-name()='Header'and " + envelopNamespaceCondition + "]";
         internal const string SignatureName = "Signature";
 
         internal const string GetStateMethodName = "getState";
